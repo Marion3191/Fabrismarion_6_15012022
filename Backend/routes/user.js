ab3db33fc512f0ff20efcad09d04b42a2866d888
Marion3191 <fbsmarion@gmail.com>
2022-02-15 10:22:55 +0100

    securiter password + helmet

--- Backend/routes/user.js
+++ Backend/routes/user.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const userCtrl = require('../controllers/user');
+const Password = require("../middleware/Password")
 
-router.post('/signup', userCtrl.signup);
+
+router.post('/signup', Password,  userCtrl.signup);
 router.post('/login', userCtrl.login);
 
 module.exports = router;
